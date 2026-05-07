bd1a3eca921b8b583c01d34572eb3746d14a928d
Alan <king.alan44@gmail.com>
2026-05-07 23:08:04 +0200

    feat : loading

--- src/components/screens/LoadingScreen.jsx
+++ src/components/screens/LoadingScreen.jsx
@@ -2,23 +2,32 @@ import { useProgress } from "@react-three/drei";
 import { useState } from "react";
 
 export function LoadingScreen() {
-	const { progress, active } = useProgress();
-	const [isHidden, setIsHidden] = useState(false);
+    const { progress, active } = useProgress();
+    const [isHidden, setIsHidden] = useState(false);
 
-	const isLoaded = !active && progress === 100;
+    const isLoaded = !active && progress === 100;
 
-	if (isHidden) return null;
+    if (isHidden) return null;
 
-	return (
-		<div
-			className={`loading__screen ${isLoaded ? "finished" : ""}`}
-			onTransitionEnd={() => {
-				if (isLoaded) {
-					setIsHidden(true);
-				}
-			}}
-		>
-			<strong>LOADING</strong>
-		</div>
-	);
+    return (
+        <div
+            className={`loading__screen ${isLoaded ? "finished" : ""}`}
+            onTransitionEnd={() => {
+                if (isLoaded) {
+                    setIsHidden(true);
+                }
+            }}
+        >
+            <div className="loading__container">
+                <img 
+                    src="/assets/images/loading/Loading.png" 
+                    alt="Loading..." 
+                    className="loading__spinner" 
+                />
+                <div className="loading__percentage">
+                    {Math.round(progress)}%
+                </div>
+            </div>
+        </div>
+    );
 }
